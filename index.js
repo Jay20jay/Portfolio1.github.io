79d2072e6fc93ae65833704758d58817739d848f
Jay20jay <101285920+Jay20jay@users.noreply.github.com>
2024-10-22 13:45:36 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hi my name is Joe. I'm from Nigeria but I have italian and greek heritage. I moved to the UK when I was 14 to futher my education.";
+  }
+
+window.alert(Welcometomywebsite);
+
